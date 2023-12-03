a7f60ab7f509ab84381d0bc1be41af59be04550f
Goel Nkoko <goelnkoko12@hotmail.com>
2023-12-03 23:25:00 +0100

    Main view controller

---
+++ src/main/java/com/nkumbo/workshopjavafxjbdc/gui/MainViewController.java
@@ -0,0 +1,39 @@
+package com.nkumbo.workshopjavafxjbdc.gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemSeller;
+
+    @FXML
+    private MenuItem menuItemDepartment;
+
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    private void onMenuItemSellerAction(){
+        System.out.println("Seller");
+    }
+
+    @FXML
+    private void onMenuItemDepartmentAction(){
+        System.out.println("Department");
+    }
+
+    @FXML
+    private void onMenuItemAboutAction(){
+        System.out.println("About");
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
